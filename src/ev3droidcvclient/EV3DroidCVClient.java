bffb6f281f6512cd7b443cc04a9c0f64e81587b9
Rics <richard_szabo@jataka.hu>
2017-01-28 11:22:41 +0100

    Disconnect after a button press. DrawRow utility function with clearRow and drawString. Cleanup of unused codes.
    All untested TODO test

--- src/ev3droidcvclient/EV3DroidCVClient.java
+++ src/ev3droidcvclient/EV3DroidCVClient.java
@@ -1,10 +1,8 @@
 package ev3droidcvclient;
 
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import lejos.hardware.Audio;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.Button;
 import lejos.hardware.Keys;
@@ -22,10 +20,8 @@ public class EV3DroidCVClient {
 
     private Socket socket;
     private DataInputStream in;
-    private DataOutputStream out;
     private final EV3 ev3 = (EV3) BrickFinder.getLocal();
     private final TextLCD lcd = ev3.getTextLCD();
-    private final Audio audio = ev3.getAudio();
     private final Keys keys = ev3.getKeys();
     private final RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
     private final RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
@@ -43,20 +39,23 @@ public class EV3DroidCVClient {
     String getIP() {
         return (isBluetooth ? btIPPrefix : wifiIPPrefix) + ipEnd;
     }
-    
+
+    void drawRow(String string, int row) {
+        lcd.clear(row);
+        lcd.drawString(string, 0, row);        
+    }
+        
     void setMode(boolean isBluetooth) {
         ipDomain = (isBluetooth ? 0 : 100);
-        lcd.clear(MODE_ROW);
-        lcd.drawString(isBluetooth? "Bluetooth" : "Wi-Fi", 0, MODE_ROW);                
-        lcd.clear(IP_ROW);
-        lcd.drawString(getIP(), 0, IP_ROW);
+        drawRow(isBluetooth? "Bluetooth" : "Wi-Fi", MODE_ROW);                
+        drawRow(getIP(), IP_ROW);
     }
-    
+        
     void init () {
-        lcd.drawString("IP:", 0, IP_ROW-1);
-        lcd.drawString("Status:", 0, STATUS_ROW-1);
-        lcd.drawString("Setting IP", 0, STATUS_ROW);
+        drawRow("IP:", IP_ROW-1);
+        drawRow("Status:", STATUS_ROW-1);
         for(;;) {
+            drawRow("Setting IP", STATUS_ROW);
             setMode(isBluetooth);
             int but = Button.waitForAnyPress();
             if( (but & Button.ID_ESCAPE) != 0 ) {
@@ -67,8 +66,8 @@ public class EV3DroidCVClient {
                     try {
                         run();
                     } catch (IOException e) {
-                        lcd.drawString("Disconnected",0,STATUS_ROW);
-                        lcd.drawString("E:" + e.getMessage(), 0, STATUS_ROW+1);
+                        drawRow("Disconnected",STATUS_ROW);
+                        drawRow("E:" + e.getMessage(), STATUS_ROW+1);
                     }
                 }
             } else if( (but & Button.ID_UP) != 0 ) {
@@ -84,25 +83,38 @@ public class EV3DroidCVClient {
     
     boolean connect () {
         try {
-            lcd.drawString("Connecting...", 0, STATUS_ROW);
+            drawRow("Connecting...", STATUS_ROW);
             socket = new Socket(getIP(), 1234);
-            out = new DataOutputStream(socket.getOutputStream());
             in = new DataInputStream(socket.getInputStream());
-            lcd.drawString("Connected    ", 0, STATUS_ROW);
+            drawRow("Connected", STATUS_ROW);
             return true;
         } catch (IOException e) {
-            lcd.drawString("E:" + e.getMessage(), 0, STATUS_ROW);
+            drawRow("E:" + e.getMessage(), STATUS_ROW);
             keys.waitForAnyPress();
         }        
         return false;
     }
 
+    boolean disconnect () {
+        try {
+            drawRow("Disconnecting", STATUS_ROW);
+            socket.close();
+            drawRow("Disconnected", STATUS_ROW);
+            return true;
+        } catch (IOException e) {
+            drawRow("E:" + e.getMessage(), STATUS_ROW);
+            keys.waitForAnyPress();
+        }        
+        return false;
+    }
+    
     void run() throws IOException {
         leftMotor.synchronizeWith(new RegulatedMotor[]{rightMotor});
-        while (true) {
+        boolean finish = false;
+        while (!finish) {
             double x = in.readDouble();            
             leftMotor.startSynchronization();
-            lcd.drawString("dir: " + String.format("%6.2f", x), 0, STATUS_ROW+1);
+            drawRow("dir: " + String.format("%6.2f", x), STATUS_ROW+1);
             if( x < - 50 ) {
                 leftMotor.stop(); 
                 rightMotor.stop(); 
@@ -113,6 +125,10 @@ public class EV3DroidCVClient {
                 rightMotor.forward();
             }
             leftMotor.endSynchronization();
+            if( Button.waitForAnyPress(10) != 0 ) {
+                disconnect();
+                finish = true;
+            }
         }
     }
     
